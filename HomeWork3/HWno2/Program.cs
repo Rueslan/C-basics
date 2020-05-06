125ca050dd01097f58c9d9bdaf69f23b98ade2b1
Rueslan <krus1501@mail.ru>
2020-05-07 01:49:16 +0400

    HomeWork-3

---
+++ HomeWork3/HWno2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWno2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа подсчёта суммы нечётных положительных чисел.\n");
+
+            Input();
+            
+            Console.ReadKey();
+        }
+
+        static void Input()
+        {
+            bool result;
+            string arr="";
+            int num, sum = 0;
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                result = Int32.TryParse(Console.ReadLine(), out num);
+                if (result == false)
+                {
+                    Console.WriteLine("Ошибка! Не удается преобразовать строку в число!");
+                    continue;
+                } 
+                if (num == 0)
+                {
+                    break;
+                }
+                else if (num % 2 != 0 && num > 0)
+                {
+                    sum += num;
+                }
+                arr +=num+" ";
+            }
+            Console.WriteLine($"\nВведённые числа: {arr}");
+            Console.WriteLine($"Сумма нечётных положительных чисел: {sum}");
+        }
+    }
+}
---
+++ HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class ComplexClass
+    {
+        public double im;
+        public double re;
+        public ComplexClass Plus(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+        public ComplexClass Multi(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        public ComplexClass Subtract(ComplexClass x)
+        {
+            ComplexClass y = new ComplexClass();
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+        public override string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+    struct Complex
+    {
+        public double im;
+        public double re;
+        public Complex Plus(Complex x)
+        {
+            Complex y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+        public Complex Multi(Complex x)
+        {
+            Complex y;
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        public Complex Subtract(Complex x)
+        {
+            Complex y;
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+        public override string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа демонстрации работы методов с комплексными числами.");
+
+            byte way = 1;
+            bool wayResult = false;
+            while (true)
+            {
+                Console.WriteLine("\nВыберите способ работы программы:\n\t1 - Использовать класс\n\t2 - Использовать структуру");
+                Console.Write("\nВвод: ");
+                wayResult = Byte.TryParse(Console.ReadLine(), out way);
+
+                if (wayResult && (way == 1 || way == 2))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода!");
+                    continue;
+                }
+            }
+
+
+            Console.WriteLine();
+            switch (way)
+            {
+                case 1:
+                    Console.WriteLine("\nВычисление, используя класс.");
+                    ComplexClass complex1 = new ComplexClass();
+                    complex1.re = 4;
+                    complex1.im = 4;
+
+                    ComplexClass complex2 = new ComplexClass();
+                    complex2.re = 2;
+                    complex2.im = 2;
+
+                    ComplexClass result = complex1.Plus(complex2);
+                    Console.WriteLine(result.ToString());
+                    result = complex1.Multi(complex2);
+                    Console.WriteLine(result.ToString());
+                    result = complex1.Subtract(complex2);
+                    Console.WriteLine(result.ToString());
+                    break;
+                case 2:
+                    Console.WriteLine("\nВычисление, используя структуру.");
+                    Complex complex_1;
+                    complex_1.re = 4;
+                    complex_1.im = 4;
+
+                    Complex complex_2;
+                    complex_2.re = 2;
+                    complex_2.im = 2;
+
+                    Complex result2 = complex_1.Plus(complex_2);
+                    Console.WriteLine(result2.ToString());
+                    result2 = complex_1.Multi(complex_2);
+                    Console.WriteLine(result2.ToString());
+                    result2 = complex_1.Subtract(complex_2);
+                    Console.WriteLine(result2.ToString());
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
