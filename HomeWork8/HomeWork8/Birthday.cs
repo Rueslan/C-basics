d8aafcb3fbd0fd69b4ad00c9cee0fa965dacda09
Rueslan <krus1501@mail.ru>
2020-05-26 12:38:31 +0400

    HomeWork-8

---
+++ HomeWork8/HomeWork8/Birthday.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    [Serializable]
+    public class Birthday
+    {
+        public string name;
+        public DateTime date;
+        public Birthday() { }
+        public Birthday(string name,DateTime date)
+        {
+            this.name = name;
+            this.date = date;
+        }
+    }
+}
